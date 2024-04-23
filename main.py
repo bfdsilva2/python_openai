4d6a20c0a511284635c66e383b1126012d5b37dd
bfernando <bfernando@eservicios.indracompany.com>
2024-04-23 18:22:43 +0100

    main2

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+resposta = cliente.chat.completions.create(
+    messages=[
+        {
+            "role":"system",
+            "content":"Liste apenas produtos em um parágrafo"
+        },
+        {
+            "role":"user",
+            "content":"Liste produtos sustentáveis"
+        }
+    ],
+    model="gpt-3.5-turbo"
+)
+
+print(resposta.choices[0].message.content)
